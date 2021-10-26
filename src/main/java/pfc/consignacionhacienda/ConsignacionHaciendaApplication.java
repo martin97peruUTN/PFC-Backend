dd126fb30841d4fefbb79f94aee274494dbcce17
Martin Perussini <mperussini@frsf.utn.edu.ar>
2021-10-26 16:21:08 -0300

    First Commit

---
+++ src/main/java/pfc/consignacionhacienda/ConsignacionHaciendaApplication.java
@@ -0,0 +1,13 @@
+package pfc.consignacionhacienda;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConsignacionHaciendaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConsignacionHaciendaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pfc/consignacionhacienda/ConsignacionHaciendaApplicationTests.java
@@ -0,0 +1,13 @@
+package pfc.consignacionhacienda;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConsignacionHaciendaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
