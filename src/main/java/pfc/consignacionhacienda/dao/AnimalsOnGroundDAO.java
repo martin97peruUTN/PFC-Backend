c9f96f109f0236adb7b807438a4ab6ad74e68f5a
Martin Perussini <mperussini@frsf.utn.edu.ar>
2021-12-28 12:12:37 -0300

    Fix

--- src/main/java/pfc/consignacionhacienda/dao/AnimalsOnGroundDAO.java
+++ src/main/java/pfc/consignacionhacienda/dao/AnimalsOnGroundDAO.java
@@ -13,21 +13,21 @@ import java.util.List;
 @Repository
 public interface AnimalsOnGroundDAO extends JpaRepository<AnimalsOnGround, Integer> {
 
-    @Query("SELECT ag FROM Batch b JOIN b.animalsOnGround ag JOIN b.auction au WHERE au.id=:auctionId AND (ag.deleted IS NULL OR ag.deleted IS FALSE) AND (b.deleted IS NULL OR b.deleted IS FALSE) AND (au.deleted IS NULL OR au.deleted IS FALSE) ORDER BY ag.startingOrder, ag.id DESC")
+    @Query("SELECT ag FROM Batch b JOIN b.animalsOnGround ag JOIN b.auction au WHERE au.id=:auctionId AND (ag.deleted IS NULL OR ag.deleted IS FALSE) AND (b.deleted IS NULL OR b.deleted IS FALSE) AND (au.deleted IS NULL OR au.deleted IS FALSE) ORDER BY ag.startingOrder")
     Page<AnimalsOnGround> getAnimalsOnGroundByAuction(Integer auctionId, Pageable of);
 
-    @Query("SELECT ag FROM Batch b JOIN b.animalsOnGround ag JOIN b.auction au WHERE au.id=:auctionId AND ag.sold IS TRUE AND (ag.deleted IS NULL OR ag.deleted IS FALSE) AND (b.deleted IS NULL OR b.deleted IS FALSE) AND (au.deleted IS NULL OR au.deleted IS FALSE) ORDER BY ag.startingOrder, ag.id DESC")
+    @Query("SELECT ag FROM Batch b JOIN b.animalsOnGround ag JOIN b.auction au WHERE au.id=:auctionId AND ag.sold IS TRUE AND (ag.deleted IS NULL OR ag.deleted IS FALSE) AND (b.deleted IS NULL OR b.deleted IS FALSE) AND (au.deleted IS NULL OR au.deleted IS FALSE) ORDER BY ag.startingOrder")
     Page<AnimalsOnGround> getAnimalsOnGroundByAuctionSold(Integer auctionId, Pageable of);
 
-    @Query("SELECT ag FROM Batch b JOIN b.animalsOnGround ag JOIN b.auction au WHERE au.id=:auctionId AND ag.sold IS FALSE AND ag.notSold IS TRUE AND (ag.deleted IS NULL OR ag.deleted IS FALSE) AND (b.deleted IS NULL OR b.deleted IS FALSE) AND (au.deleted IS NULL OR au.deleted IS FALSE) ORDER BY ag.startingOrder, ag.id DESC")
+    @Query("SELECT ag FROM Batch b JOIN b.animalsOnGround ag JOIN b.auction au WHERE au.id=:auctionId AND ag.sold IS FALSE AND ag.notSold IS TRUE AND (ag.deleted IS NULL OR ag.deleted IS FALSE) AND (b.deleted IS NULL OR b.deleted IS FALSE) AND (au.deleted IS NULL OR au.deleted IS FALSE) ORDER BY ag.startingOrder")
     Page<AnimalsOnGround> getAnimalsOnGroundByAuctionNotSold(Integer auctionId, Pageable of);
 
-    @Query("SELECT ag FROM Batch b JOIN b.animalsOnGround ag JOIN b.auction au WHERE au.id=:auctionId AND ag.sold IS FALSE AND ag.notSold IS FALSE AND (ag.deleted IS NULL OR ag.deleted IS FALSE) AND (b.deleted IS NULL OR b.deleted IS FALSE) AND (au.deleted IS NULL OR au.deleted IS FALSE) ORDER BY ag.startingOrder, ag.id DESC")
+    @Query("SELECT ag FROM Batch b JOIN b.animalsOnGround ag JOIN b.auction au WHERE au.id=:auctionId AND ag.sold IS FALSE AND ag.notSold IS FALSE AND (ag.deleted IS NULL OR ag.deleted IS FALSE) AND (b.deleted IS NULL OR b.deleted IS FALSE) AND (au.deleted IS NULL OR au.deleted IS FALSE) ORDER BY ag.startingOrder")
     Page<AnimalsOnGround> getAnimalsOnGroundByAuctionForSell(Integer auctionId, Pageable of);
 
     @Query("SELECT ag FROM AnimalsOnGround ag  WHERE ag.id=:animalsId AND (ag.deleted IS NULL OR ag.deleted IS FALSE)")
     AnimalsOnGround findByIdAndNotDeleted(Integer animalsId);
 
-    @Query("SELECT ag FROM Batch b JOIN b.animalsOnGround ag JOIN b.auction au WHERE au.id=:auctionId AND ag.sold IS FALSE AND ag.notSold IS FALSE AND (ag.deleted IS NULL OR ag.deleted IS FALSE) AND (b.deleted IS NULL OR b.deleted IS FALSE) AND (au.deleted IS NULL OR au.deleted IS FALSE) ORDER BY ag.startingOrder, ag.id DESC")
+    @Query("SELECT ag FROM Batch b JOIN b.animalsOnGround ag JOIN b.auction au WHERE au.id=:auctionId AND ag.sold IS FALSE AND ag.notSold IS FALSE AND (ag.deleted IS NULL OR ag.deleted IS FALSE) AND (b.deleted IS NULL OR b.deleted IS FALSE) AND (au.deleted IS NULL OR au.deleted IS FALSE) ORDER BY ag.startingOrder")
     List<AnimalsOnGround> getAllAnimalsOnGroundByAuctionForSell(Integer auctionId);
 }
