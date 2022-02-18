750a64a81489fa4e1535f04ad2fe47245943555a
TomasRavelli <tomy_ravelli@hotmail.com>
2022-02-18 17:55:37 -0300

    enpoint para websocket por id de remate

--- src/main/java/pfc/consignacionhacienda/services/websockets/DbListener.java
+++ src/main/java/pfc/consignacionhacienda/services/websockets/DbListener.java
@@ -4,26 +4,52 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.messaging.simp.SimpMessagingTemplate;
+import org.springframework.stereotype.Component;
 import pfc.consignacionhacienda.model.SoldBatch;
+import pfc.consignacionhacienda.services.batch.BatchService;
 
-import javax.persistence.PostPersist;
-import javax.persistence.PostRemove;
-import javax.persistence.PostUpdate;
+import javax.persistence.PrePersist;
+import javax.persistence.PreRemove;
+import javax.persistence.PreUpdate;
 
+@Component
 public class DbListener {
 
     private static final Logger logger = LoggerFactory.getLogger(DbListener.class);
 
-    @Autowired
-    private SimpMessagingTemplate websocket;
+//    @Autowired
+//    private SimpMessagingTemplate websocket;
 
-    @PostPersist
-    @PostUpdate
-    @PostRemove
+    @Autowired
+//    BatchService batchService;
+    public void init(SimpMessagingTemplate websocket)
+    {
+        if(DbListener.websocket == null) {
+            DbListener.websocket = websocket;
+        }
+    }
+
+    static private SimpMessagingTemplate websocket;
+
+    @Autowired
+//    BatchService batchService;
+    public void init(BatchService batchService)
+    {
+        if(DbListener.batchService == null) {
+            DbListener.batchService = batchService;
+        }
+    }
+
+    static private BatchService batchService;
+
+
+    @PrePersist
+    @PreUpdate
+    @PreRemove
     private void postAnyUpdate(SoldBatch soldBatch) {
-        logger.debug("add/update/delete complete for user: " + soldBatch.toString());
-        this.websocket.convertAndSend(
-                WebSocketConfiguration.MESSAGE_PREFIX + "/newSoldBatch", soldBatch.toString());
+        Integer id = batchService.getBatchByAnimalsOnGroundId(soldBatch.getAnimalsOnGround().getId()).getAuction().getId();
+        websocket.convertAndSend(
+                WebSocketConfiguration.MESSAGE_PREFIX + "/newSoldBatch/"+id, soldBatch.toString());
     }
 
 }
