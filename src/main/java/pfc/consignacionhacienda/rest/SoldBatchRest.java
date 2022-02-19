57aaedfcb559215a3ac4570264cc5ff1c36a8601
TomasRavelli <tomy_ravelli@hotmail.com>
2022-02-19 17:41:40 -0300

    enpoint para websocket por id de remate. Fix: ahora anda cuando se actualiza una entidad.

--- src/main/java/pfc/consignacionhacienda/rest/SoldBatchRest.java
+++ src/main/java/pfc/consignacionhacienda/rest/SoldBatchRest.java
@@ -70,6 +70,7 @@ public class SoldBatchRest {
             logger.error(e.getMessage());
             return new ResponseEntity<>(new ErrorResponse(e.getMessage()), HttpStatus.UNAUTHORIZED);
         } catch (Exception e){
+            e.printStackTrace();
             logger.error(e.getMessage());
             return new ResponseEntity<>(new ErrorResponse(e.getMessage()), HttpStatus.INTERNAL_SERVER_ERROR);
         }
